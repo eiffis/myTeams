d938e41c549125858f59188ef4d1363355f39ae2
Gabriel Viel <gabriel.viel@epitech.eu>
2026-04-10 09:37:44 +0200

    [ADD] Team store and load functions

--- Team.hpp
+++ Team.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <vector>
 #include "Constants.hpp"
 #include "Channel.hpp"
 
@@ -11,8 +10,13 @@ private:
     char _message[MAX_DESCRIPTION_LENGTH];
     uuid_t _uuid;
     std::vector<uuid_t> _subscribedUsers;
-    std::vector<Channel> _threads;
+    std::vector<Channel> _channels;
 public:
     Team(/* args */);
     ~Team();
+
+    char *getName() { return _name; };
+    uuid_t &getUuid() { return _uuid; };
+    bool store(std::ofstream &out);
+    bool load(std::ifstream &in);
 };
