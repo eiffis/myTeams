e4a5ea7866c8cab5e26de6e1f69e79e371740535
eiffis <enzo.iffis@epitech.eu>
2026-04-14 01:03:37 +0200

    [ADD] --help for main client and server

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,11 @@
 
 int main(int ac, char **argv)
 {
+    if (ac == 2 && std::string(argv[1]) == "--help"){
+        std::string msg = "USAGE: ./myteams_server port\n\nport is the port number on which the server socket listens.\n";
+        std::cout << msg;
+        return 0;
+    }
     if (ac != 2){
         std::string msg = "USAGE: ./myteams_server port\n\nport is the port number on which the server socket listens.\n";
         std::cout << msg;
