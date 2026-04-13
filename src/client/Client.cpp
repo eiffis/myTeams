d67a34ce79726e2b28aebb8e3cf2a2f79ee0d558
eiffis <enzo.iffis@epitech.eu>
2026-04-13 02:44:20 +0200

    [FIX] client event user print

--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -24,7 +24,7 @@ void Client::Client::handleEvent(std::string fullMessage, Parser &parser)
     }
     else if (command == "EVENT_USER" && arguments.size() == 3){
         int userStatus = std::stoi(arguments[2].c_str());
-        client_print_users(arguments[0].c_str(), arguments[1].c_str(), userStatus);
+        client_print_user(arguments[0].c_str(), arguments[1].c_str(), userStatus);
     }
     else if (command == "EVENT_USER_DON'T_EXISTS" && arguments.size() == 1){
         client_error_unknown_user(arguments[0].c_str());   
