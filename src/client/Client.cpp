a98f5fe1930e18885b4718026b367e3bc9eb3411
eiffis <enzo.iffis@epitech.eu>
2026-04-13 23:35:39 +0200

    [ADD] unsubscribe command

--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -74,6 +74,9 @@ void Client::Client::handleEvent(std::string fullMessage, Parser &parser)
     else if (command == "PERSONAL_SUBSCRIBED" && arguments.size() == 2){
         client_print_subscribed(arguments[0].c_str(), arguments[1].c_str());
     }
+    else if (command == "PERSONAL_UNSUBSCRIBED" && arguments.size() == 2){
+        client_print_unsubscribed(arguments[0].c_str(), arguments[1].c_str());
+    }
     else return;
 }
 
--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -43,6 +43,40 @@ Server::Server::~Server()
     close(_serverFD);
 }
 
+void Server::Server::unsubscribeCommand(int clientFD, const std::vector<std::string> &arguments)
+{
+    if (arguments.size() != 1){
+        write(clientFD, "INVALID_ARGS.\n", 14);
+        return;
+    }
+    auto itUser = std::find_if(_users.begin(), _users.end(), [&clientFD](const User& u) {
+        return u.getFd() == clientFD;
+    });
+    if (itUser == _users.end() || !itUser->isLoggedIn()) {
+        write(clientFD, "UNAUTHORIZED\n", 13);
+        return;
+    }
+    std::string teamUuid = arguments[0];
+    auto itTeam = std::find_if(_teams.begin(), _teams.end(), [&teamUuid](const Team& t) {
+        char uuidStr[37];
+        uuid_unparse(t.getUuid().uuid, uuidStr);
+        return std::string(uuidStr) == teamUuid;
+    });
+    if (itTeam == _teams.end()) {
+        std::string msg = "EVENT_UNKNOWN_TEAM \"" + teamUuid + "\"\n";
+        write(clientFD, msg.c_str(), msg.length());
+        return;
+    }
+    char userUuidStr[37];
+    uuid_unparse(itUser->getUuid().uuid, userUuidStr);
+    if (itTeam->isUserSubscribed(userUuidStr)) {
+        itTeam->unsubscribeUser(userUuidStr);
+    }
+    server_event_user_unsubscribed(teamUuid.c_str(), userUuidStr);
+    std::string msg = "PERSONAL_UNSUBSCRIBED \"" + std::string(userUuidStr) + "\" \"" + teamUuid + "\"\n";
+    write(clientFD, msg.c_str(), msg.length());
+}
+
 void Server::Server::subscribeCommand(int clientFD, const std::vector<std::string> &arguments)
 {
     if (arguments.size() != 1){
